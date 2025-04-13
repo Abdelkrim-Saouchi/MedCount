0b36f3c88f58184b6f239117b248564a16061059
Abdelkrim Saouchi <krimouv1995@gmail.com>
2025-04-13 16:21:31 +0100

    Implement formes and unites queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
-import useDatabase from "./hooks/useDatabase";
+import Layout from "./components/Layout";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const { database, isLoading, error } = useDatabase();
-
   return (
-    <main className="flex min-h-dvh justify-center bg-gradient-to-r from-[#5C258D] to-[#4389A2] py-2 text-white">
-      <form className="h-fit space-y-3 border border-green-400 p-4">
-        <div className="flex flex-col">
-          <label htmlFor="form-select">Séléctionner la forme:</label>
-          <select id="form-select">
-            <option value="collyres">Collyres</option>
-            <option value="sol. buv">Sol. Buv</option>
-          </select>
-        </div>
-
-        <div className="flex flex-col">
-          <label>Nom de médicament:</label>
-          <input placeholder="Zomax" className="border border-green-400 p-2" />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="unity-select">Séléctionner l'unité:</label>
-          <select id="unity-select">
-            <option value="gouttes">Gouttes</option>
-            <option value="milligrames">milligrames</option>
-          </select>
-        </div>
-
-        <div className="flex flex-col">
-          <label>Capacité totale:</label>
-          <input type="number" className="border border-green-400 p-2" />
-        </div>
-
-        <button className="bg-gray-400 p-2 hover:bg-gray-800">
-          Enregistrement
-        </button>
-      </form>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,159 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import useDatabase from "../hooks/useDatabase";
+import { FormEvent } from "react";
+
+type DataForm = {
+  forme: String | null;
+  nomination: string | null;
+  unité: string | null;
+  capacité: string | null;
+};
+
+type FormeRow = {
+  forme_id: number;
+  nomination: string;
+};
+
+type UniteRow = {
+  unite_id: number;
+  nomination: string;
+};
+
+const Layout = () => {
+  const queryClient = useQueryClient();
+
+  const { database, isLoading, error } = useDatabase();
+
+  const {
+    isLoading: dataIsLoading,
+    isError,
+    error: err,
+    data,
+  } = useQuery({
+    queryKey: ["formes"],
+    enabled: !!database,
+    queryFn: async () => {
+      if (database) {
+        const formeRows: FormeRow[] = await database.select(
+          "SELECT * FROM formes",
+        );
+        const uniteRows: UniteRow[] = await database.select(
+          "SELECT * FROM unites",
+        );
+        return {
+          formeRows,
+          uniteRows,
+        };
+      }
+    },
+  });
+
+  const mutation = useMutation({
+    mutationFn: async (data: DataForm) => {
+      console.log("data:", data);
+      try {
+        await database?.execute("PRAGMA foreign_keys = ON");
+
+        await database?.execute("BEGIN");
+        let result1 = await database?.execute(
+          "INSERT INTO formes (nomination) VALUES ($1)",
+          [data.forme],
+        );
+        let result2 = await database?.execute(
+          "INSERT INTO unites (nomination) VALUES ($1)",
+          [data.unité],
+        );
+        console.log("result1", result1?.lastInsertId);
+        await database?.execute(
+          "INSERT INOT medicaments (nomination, capacité, forme, unite) VALUES ($1, $2, $3, $4)",
+          [
+            data.nomination,
+            data.capacité,
+            result1?.lastInsertId,
+            result2?.lastInsertId,
+          ],
+        );
+      } catch (error) {
+        await database?.execute("ROLLBACK");
+        console.log("transaction failed", error);
+      }
+    },
+  });
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let data = new FormData(event.currentTarget);
+    const forme = data.get("forme") as string;
+    const nomination = data.get("nomination") as string;
+    const unité = data.get("unités") as string;
+    const capacité = data.get("capacité") as string;
+
+    mutation.mutate({
+      forme,
+      nomination,
+      unité,
+      capacité,
+    });
+  };
+
+  return (
+    <main className="flex min-h-dvh justify-center bg-gradient-to-r from-[#5C258D] to-[#4389A2] py-2 text-white">
+      <form
+        onSubmit={onSubmit}
+        className="h-fit space-y-3 border border-green-400 p-4"
+      >
+        <div className="flex flex-col">
+          <label htmlFor="form-select">Séléctionner la forme:</label>
+          <select id="form-select" name="forme">
+            {!dataIsLoading &&
+              data?.formeRows.map((forme) => {
+                return (
+                  <option key={forme.forme_id} value={forme.nomination}>
+                    {forme.nomination}
+                  </option>
+                );
+              })}
+          </select>
+          {isError && <p className="text-red-600">{err.message}</p>}
+        </div>
+
+        <div className="flex flex-col">
+          <label>Nom de médicament:</label>
+          <input
+            placeholder="Zomax"
+            name="nomination"
+            className="border border-green-400 p-2"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="unity-select">Séléctionner l'unité:</label>
+          <select id="unity-select" name="unités">
+            {!dataIsLoading &&
+              data?.uniteRows.map((unite) => (
+                <option key={unite.unite_id} value={unite.nomination}>
+                  {unite.nomination.toUpperCase()}
+                </option>
+              ))}
+          </select>
+          {isError && <p className="text-red-600">{err.message}</p>}
+        </div>
+
+        <div className="flex flex-col">
+          <label>Capacité totale:</label>
+          <input
+            type="number"
+            name="capacité"
+            className="border border-green-400 p-2"
+          />
+        </div>
+
+        <button type="submit" className="bg-gray-400 p-2 hover:bg-gray-800">
+          Enregistrement
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default Layout;
