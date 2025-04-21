c2496000937fc2e14ab47cd42122fdddf0794def
Abdelkrim Saouchi <krimouv1995@gmail.com>
2025-04-21 16:19:50 +0100

    Make unitList reload when new unit is added

--- src/components/CreateUnitForm.tsx
+++ src/components/CreateUnitForm.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import useDatabase from "../hooks/useDatabase";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const CreateUnitForm = () => {
+  const queryClient = useQueryClient();
   const [unitName, setUnitName] = useState("");
   const { database } = useDatabase();
 
@@ -18,6 +19,7 @@ const CreateUnitForm = () => {
     },
     onSuccess: () => {
       setUnitName("");
+      queryClient.invalidateQueries();
     },
   });
 
