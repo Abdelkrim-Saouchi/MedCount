7d66091a285746933dfd86e191d401f1ea70d59b
Abdelkrim Saouchi <krimouv1995@gmail.com>
2025-05-04 14:57:53 +0100

    Add quit event listener to tray icon list

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -36,12 +36,21 @@ pub fn run() {
 
     tauri::Builder::default()
         .setup(|app| {
-            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let quit_i = MenuItem::with_id(app, "quit", "Quiter", true, None::<&str>)?;
             let menu = Menu::with_items(app, &[&quit_i])?;
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
                 .show_menu_on_left_click(true)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        println!("quit menu item was clicked");
+                        app.exit(0);
+                    }
+                    _ => {
+                        println!("menu item {:?} not handled", event.id);
+                    }
+                })
                 .build(app)?;
 
             Ok(())
